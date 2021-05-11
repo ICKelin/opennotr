ae2ff3286700e9ae9b3de6a50cdb3662558d7d89
ICKelin <yingjiu.hulu@gmail.com>
2021-05-11 22:35:07 +0800

    feature: add dummy plugin

---
+++ opennotrd/plugin/dummy/dummy.go
@@ -0,0 +1,24 @@
+package dummy
+
+import (
+	"encoding/json"
+
+	"github.com/ICKelin/opennotr/opennotrd/plugin"
+	"github.com/ICKelin/opennotr/pkg/logs"
+)
+
+func init() {
+	plugin.Register("dummy", &DummyPlugin{})
+}
+
+type DummyPlugin struct{}
+
+func (d *DummyPlugin) Setup(cfg json.RawMessage) error {
+	return nil
+}
+
+func (d *DummyPlugin) RunProxy(meta *plugin.PluginMeta) error {
+	logs.Info("dummy plugin client config: %v", meta.Ctx)
+	return nil
+}
+func (d *DummyPlugin) StopProxy(meta *plugin.PluginMeta) {}
--- opennotrd/plugins.go
+++ opennotrd/plugins.go
@@ -2,6 +2,7 @@ package opennotrd
 
 import (
 	// plugin import
+	_ "github.com/ICKelin/opennotr/opennotrd/plugin/dummy"
 	_ "github.com/ICKelin/opennotr/opennotrd/plugin/restyproxy"
 	_ "github.com/ICKelin/opennotr/opennotrd/plugin/tcpproxy"
 	_ "github.com/ICKelin/opennotr/opennotrd/plugin/udpproxy"
